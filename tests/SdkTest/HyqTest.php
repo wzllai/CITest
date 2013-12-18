e1ce24a5bce4b62a4c83380970fdfe092f3697f5
hyq <funyq@foxmail.com>
2013-12-18 18:38:36 +0800

    debug

--- tests/SdkTest/HyqTest.php
+++ tests/SdkTest/HyqTest.php
@@ -3,6 +3,6 @@ require  __DIR__ . "/../../src/sdk/Hyq.php";
 class HyqTest extends  PHPUnit_Framework_TestCase {
         public function testMyfunc() {
 			$obj = new Hyq();
-			$this->assertEquals(1, $obj->myfunc());
+			$this->assertEquals(2, $obj->myfunc());
 		}
 }
